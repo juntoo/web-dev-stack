eb03aedd4b3428509aaeb69833bc6340257561bf
juntoo <juntoo2249@gmail.com>
2025-05-28 21:59:34 +0900

    실습06 업데이트

--- 03_JavaScript/실습06_그림맞추기.js
+++ 03_JavaScript/실습06_그림맞추기.js
@@ -1,12 +1,11 @@
-const click = document.querySelector(".click");
+const click = document.querySelector("#click");
 const restart = document.querySelector("#restart");
 const imgContainer = document.querySelector("#imgContainer");
 const imgs = document.querySelectorAll("#imgContainer img");
 const celebration = document.querySelector("#celebration");
 
-console.log(imgs.length);
-
 let clickCount = 0;
+
 const changeImg = (e) => {
   for (let i = 0; i < imgs.length; i++) {
     let random = Math.floor(Math.random() * 3);
@@ -15,27 +14,27 @@ const changeImg = (e) => {
   clickCount += 1;
 
   click.textContent = `Click ${clickCount}`;
-};
-
-const checking = (e) => {
   const img1 = imgs[0].getAttribute("src");
   const img2 = imgs[1].getAttribute("src");
   const img3 = imgs[2].getAttribute("src");
-  console.log(img1);
-  console.log(img2);
-  console.log(img3);
+
   if (img1 == img2 && img1 == img3) {
-    console.log("help!");
     celebration.textContent =
       "축하합니다! 다시 시작하려면 재시작 버튼을 눌러주세요";
-    click.removeAttribute("class");
+    click.style.pointerEvents = "none";
   }
 };
 
-const restartGame = (e) => {};
+const restartGame = (e) => {
+  for (let i = 0; i < imgs.length; i++) {
+    imgs[i].setAttribute("src", `assets/spy${i + 1}.jpg`);
+  }
+  celebration.textContent = "";
+  click.style.pointerEvents = "";
+  click.textContent = "Click";
+  clickCount = 0;
+};
 
-click.addEventListener("click", () => {
-  changeImg(), checking();
-});
+click.addEventListener("click", changeImg);
 
 restart.addEventListener("click", restartGame);
