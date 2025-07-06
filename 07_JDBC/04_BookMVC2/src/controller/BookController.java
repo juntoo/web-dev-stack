5b31cb96e9a70085e0263adcd3b61555c02367a8
juntoo <juntoo2249@gmail.com>
2025-07-06 23:50:29 +0900

    25/07/06

---
+++ 07_JDBC/04_BookMVC2/src/controller/BookController.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dao.BookDAO;
+import dao.RentDAO;
+import vo.Book;
+
+public class BookController {
+	
+	BookDAO dao = BookDAO.getInstance();
+	RentDAO rentDao = RentDAO.getInstance();
+
+	// 1. 전체 책 조회
+	public ArrayList<Book> printBookAll() {
+		try {
+			return dao.printBookAll();
+		} catch (SQLException e) {
+			return null;
+		}
+	}
+	
+	// 2. 책 등록
+	public boolean registerBook(String title, String author, int accessAge) {
+		// 기존 제목, 저자, 제한 나이까지 동일한 책이 있으면 안되게!
+		try {
+			if(dao.checkBook(title, author, accessAge)) {
+				return false;
+			}
+			dao.registerBook(title, author, accessAge);
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+		
+	}
+	
+	// 3. 책 삭제
+	public boolean sellBook(String title) {
+		
+		try {
+			int bookNo = dao.searchBook(title);
+			
+			// 빌려있는 책은 삭제 못하게!
+			if(rentDao.selectRent(bookNo) > 0) {
+				return false;
+			}
+			int result = dao.sellBook(bookNo);
+			if(result==1) return true;
+			return false;
+		} catch (SQLException e) {
+			return false;
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ 07_JDBC/04_BookMVC2/src/dao/RentDAO.java
@@ -0,0 +1,104 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import config.ServerInfo;
+import vo.Book;
+import vo.Rent;
+
+public class RentDAO {
+
+	private static RentDAO instance = new RentDAO();
+
+	private RentDAO() {
+		try {
+			Class.forName(ServerInfo.DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static RentDAO getInstance() {
+		return instance;
+	}
+
+	public Connection connect() throws SQLException {
+		return DriverManager.getConnection(ServerInfo.URL, ServerInfo.USER, ServerInfo.PASSWORD);
+	}
+
+	// 7. 책 대여
+	public void rentBook(String id, int bookNo) throws SQLException {
+		Connection connect = connect();
+		String query = "INSERT INTO rent(id, book_no) VALUES(?, ?)";
+		PreparedStatement ps = connect.prepareStatement(query);
+		ps.setString(1, id);
+		ps.setInt(2, bookNo);
+		ps.executeUpdate();
+	}
+
+	// 8. 내가 대여한 책 조회
+	public ArrayList<Rent> printRentBook(String id) throws SQLException {
+		Connection connect = connect();
+		String query = "SELECT * FROM rent JOIN book USING(book_no) WHERE id = ?";
+		PreparedStatement ps = connect.prepareStatement(query);
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		ArrayList<Rent> list = new ArrayList<>();
+		while(rs.next()) {
+			Rent rent = new Rent();
+			rent.setRentNo(rs.getInt("rent_no"));
+			rent.setId(rs.getString("id"));
+			rent.setBookNo(rs.getInt("book_no"));
+			rent.setRentDate(rs.getDate("rent_date").toLocalDate());
+			rent.setBook(new Book(rs.getInt("book_no"), rs.getString("title"), 
+									rs.getString("author"), rs.getInt("access_age")));
+			list.add(rent);
+		}
+		return list;
+	}
+
+	// 내가 대여한 책 제목으로 rent_no값 조회
+	public int selectRent(String title, String id) throws SQLException {
+		Connection connect = connect();
+		
+		String query = "SELECT rent_no FROM rent JOIN book USING(book_no) WHERE title = ? AND id = ?";
+		PreparedStatement ps = connect.prepareStatement(query);
+		ps.setString(1, title);
+		ps.setString(2, id);
+		
+		ResultSet rs = ps.executeQuery();
+		if(rs.next()) {
+			return rs.getInt("rent_no");
+		}
+		return -1;
+	}
+	
+	// 9. 대여 취소
+	public void deleteRent(int rentNo) throws SQLException {
+		Connection connect = connect();
+		String query = "DELETE FROM rent WHERE rent_no = ?";
+		PreparedStatement ps = connect.prepareStatement(query);
+		ps.setInt(1, rentNo);
+		ps.executeUpdate();
+	}
+	
+	public int selectRent(int bookNo) throws SQLException {
+		Connection connect = connect();
+		
+		String query = "SELECT count(*) as count FROM rent WHERE book_no = ?";
+		PreparedStatement ps = connect.prepareStatement(query);
+		ps.setInt(1, bookNo);
+		ResultSet rs = ps.executeQuery();
+		if(rs.next()) {
+			return rs.getInt("count");
+		}
+		return -1;
+	}
+
+}
